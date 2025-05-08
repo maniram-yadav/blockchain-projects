67efc857ab6a1f2bf094b1e230eca4102b63fbcd
maniram <maniramyadav786@gmail.com>
2025-05-08 02:29:29 +0000

    sum functionality impemntation

---
+++ math-opertaion/src/sum/src/lib.rs
@@ -0,0 +1,42 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo}, 
+    entrypoint, 
+    entrypoint::ProgramResult, 
+    msg, 
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct MathOperation {
+    pub sum : u32,
+}
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id : &Pubkey,
+    accounts : &[AccountInfo],
+    instruction_data :&[u8]
+) -> ProgramResult {
+    let accounts_iter =  &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+    
+    if account.owner != program_id {
+        msg!("Account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    msg!("Debug Output:");
+    msg!("Account id : {} ",account.key);
+    msg!("Executable ? : {} ",account.executable);
+    msg!("Lamports : {:#?} ",account.lamports);
+    msg!("Debug output complete.");
+    msg!("Adding 1 to sum...");
+
+    let mut math_operation = MathOperation::try_from_slice(&account.data.borrow())?;
+    math_operation.sum += 1;
+    math_operation.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    msg!("Current sum is now: {}", math_operation.sum);
+    Ok(())
+}
