d6443d87419cfec526bfc79aafc068d42523daab
maniram <maniramyadav786@gmail.com>
2025-05-08 07:36:23 +0000

    initialize transaction added

--- p2-voting/anchor/voting/tests/voting.ts
+++ p2-voting/anchor/voting/tests/voting.ts
@@ -1,16 +1,63 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Voting } from "../target/types/voting";
+import { PublicKey } from '@solana/web3.js';
+
 
 describe("voting", () => {
-  // Configure the client to use the local cluster.
+
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.voting as Program<Voting>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("initializePoll", async () => {
+    
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("poll"),new anchor.BN(1).toArrayLike(Buffer,"le",8)],
+      program.programId
+    )
+    
+    const tx = await program.methods.initializePoll(
+      new anchor.BN(1),
+      new anchor.BN(0),
+      new anchor.BN(1759508293),
+      "test-poll",
+      "description",
+    )
+    .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  
+  it('initialize candidates', async () => { 
+    
+    const pollIdBuffer = new anchor.BN(1).toArrayLike(Buffer,"le",8)
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("poll"),pollIdBuffer],
+      program.programId
+    );
+    const smoothTx = await program.methods.initializeCandidate(
+      new anchor.BN(1),
+      "smooth",
+    ).accounts({
+      pollAccount:pollAddress
+    })
+    .rpc();
+    const crunchyTx = await program.methods.initializeCandidate(
+      new anchor.BN(1),
+      "crunchy",
+    ).accounts({
+      pollAccount:pollAddress
+    })
+    .rpc();
+    console.log("Your transaction complete",smoothTx);
+    console.log("Your transaction complete",crunchyTx);
+    
+  }
+
 });
