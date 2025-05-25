2eefd574aed403b67400b4c5e45c6a7e7954cc2c
maniram <maniramyadav786@gmail.com>
2025-05-25 05:12:48 +0000

    package file added

---
+++ ethereum-ctr/contracts/hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+};
+
+export default config;
